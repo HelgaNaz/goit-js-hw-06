6bc949641cb8499fc99976c566aad361d91aba6d
HelgaNaz <onworkit@gmail.com>
2021-11-11 19:01:41 +0200

    исправляет task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,12 +7,21 @@ const ingredients = [
   'Condiments',
 ];
 const list = document.querySelector("#ingredients");
+let x = []
 
 for (let i = 0; i < ingredients.length; i++) {
-  let li = document.createElement('li');
+  let li = document.createElement("li");
   li.className = "item";
   li.textContent = ingredients[i];
-  list.append(li);
+  x.push(li)
 }
+console.log(x)
+list.append(...x)
 
+// Вариант 2
+// const markup = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join("");
 
+// console.log(markup);
+// list.innerHTML = markup;
